a323f3854c6d1ecca2b1acc3781078d4f72a82ab
mostafa.safaie <mostafa.safaie@gmail.com>
2020-10-07 21:20:10 +0200

    0

--- tests.py
+++ tests.py
@@ -2,7 +2,15 @@ import os, sys
 from pathlib import Path
 from RatTag.Root import Root
 import RatTag.session as session
+import RatTag.TagFile as TagFile
+import RatTag.Profile as Profile
 
 
-s = session.get_session_profile(Root(), 'Rat111', 'Rat111_2016_08_01_13_37')
+root = Root()
+profile = Profile.Profile(root=root)
+# profile.Speed = '10.0'
+# profile.Event = 'SpeedChange'
+tagfile = TagFile.TagFile(root, 'Rat111')
+s = session.get_session_list(tagfile, profile)
+# s = tagfile.read_tag_table()
 print(s)
