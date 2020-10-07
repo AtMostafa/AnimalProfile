b0e17e5c346e1df785772a967fa97503630a407e
mostafa.safaie <mostafa.safaie@gmail.com>
2020-10-08 00:22:03 +0200

    add batch_get_animal_list

--- RatTag/session/batchAnimals.py
+++ RatTag/session/batchAnimals.py
@@ -1,4 +1,4 @@
-__all__ = ('batch_get_session_list',)
+__all__ = ('batch_get_session_list', 'batch_get_animal_list')
 from .. import Root
 from .. import TagFile
 from .. import Profile
@@ -16,8 +16,8 @@ def batch_get_session_list(root: Root.Root,
         profile = Profile.Profile(root=root)
 
     if animalList is None or animalList == '' or animalList == []:
-        animalPaths = sorted(root.glob(f'{self._prefix}???/'))
-        animalList = [k.name for animal in animalPaths]
+        animalPaths = sorted(root.glob(f'{profile._prefix}???/'))
+        animalList = [animal.name for animal in animalPaths]
 
     profileOut = Profile.Profile(root=root)
     for animal in animalList:
@@ -25,3 +25,20 @@ def batch_get_session_list(root: Root.Root,
         sessionProfile = get_session_list(tagFile, profile)
         profileOut += sessionProfile
     return profileOut
+
+
+def batch_get_animal_list(root: Root.Root, profile: Profile.Profile = None):
+    """
+    this function returns list of animals with at least one session matching the "profile"
+    """
+    if profile is None:
+        profile = Profile.Profile(root=root)
+
+    allProfiles = batch_get_session_list(root, animalList=None, profile=profile)
+    sessionList = allProfiles.Sessions
+
+    animalList = []
+    for session in sessionList:
+        animalList.append(session[:len(profile._prefix) + 3])
+    animalList = list(set(animalList))
+    return animalList
