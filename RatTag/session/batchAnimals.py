40264b723a672f676dbc7cb1c1c6363f414878f8
mostafa.safaie <mostafa.safaie@gmail.com>
2020-10-08 00:15:18 +0200

    fix batch_get_session_list

--- RatTag/session/batchAnimals.py
+++ RatTag/session/batchAnimals.py
@@ -1,32 +1,27 @@
-__all__ = (,)
+__all__ = ('batch_get_session_list',)
 from .. import Root
 from .. import TagFile
 from .. import Profile
 from .singleAnimal import *
 
 
-def batch_get_session_list(root,animalList=None,profile={},until_date=''):
+def batch_get_session_list(root: Root.Root,
+                        animalList: list = None,
+                        profile: Profile.Profile = None):
     """
     This function returns list of sessions with certain 'profile' for all the animals
-    in animalList. if animalList=[], it will search all the animals
+    in animalList. if animalList=Nonr, it will search all the animals.
     """
-    clearScreen=False
-    if animalList is None or animalList=='' or animalList==[]:
-        animalPaths=glob.glob(os.path.join(root,'Rat*'))
-        animalList=[os.path.basename(animalPaths[i]) for i,_ in enumerate(animalPaths)]
-        clearScreen=True
-    animalList=sorted(animalList)
-    sessionList=[]
-    profileDict={}
+    if profile is None:
+        profile = Profile.Profile(root=root)
+
+    if animalList is None or animalList == '' or animalList == []:
+        animalPaths = sorted(root.glob(f'{self._prefix}???/'))
+        animalList = [k.name for animal in animalPaths]
+
+    profileOut = Profile.Profile(root=root)
     for animal in animalList:
-        tagPath=os.path.join(root,animal,animal+'.tag')
-        sessionProfile=get_session_list(tagFile=tagPath,profile=profile,until_date=until_date)
-        for key in sessionProfile:
-            if key not in profileDict:
-                profileDict[key]=[]
-            profileDict[key].extend(sessionProfile[key])
-    
-    if clearScreen:
-        clear_output()
-        
-    return profileDict
+        tagFile = TagFile.TagFile(root, animal)
+        sessionProfile = get_session_list(tagFile, profile)
+        profileOut += sessionProfile
+    return profileOut
