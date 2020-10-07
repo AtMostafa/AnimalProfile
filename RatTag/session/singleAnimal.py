4de068ad779721ef0f33f59c0eacef001cef8a99
mostafa.safaie <mostafa.safaie@gmail.com>
2020-10-07 23:17:05 +0200

    add annotations

--- RatTag/session/singleAnimal.py
+++ RatTag/session/singleAnimal.py
@@ -22,7 +22,7 @@ def get_session_profile(root: Root.Root, animal: str, session: str):
     return Profile.Profile(root=root).from_dict(profile)
 
 
-def get_session_list(tagFile, profile=None):
+def get_session_list(tagFile: TagFile.TagFile, profile: Profile.Profile = None):
     """
     This function returns the list of the sessions within a tag file 
     meeting all the conditions in 'profile', MOST keys in 'profile' could be a list of accepted conditions
