160fdbe8f9a9f30191ec1098339f0363b63914b8
mostafa.safaie <mostafa.safaie@gmail.com>
2020-10-08 18:32:47 +0200

    test EventProfile

--- tests.py
+++ tests.py
@@ -7,12 +7,14 @@ import RatTag.Profile as Profile
 
 
 root = Root()
-profile = root.get_profile()
-profile.Speed = '150'
+profile1 = root.get_profile()
+profile1.Speed = '10'
+profile2 = root.get_profile()
+profile2.Speed = '15'
 # profile.Event = 'SpeedChange'
-profile.rewardType = 'Progressive'
-tagfile = TagFile.TagFile(root, 'Rat111')
-s = session.batch_get_session_list(root, ['Rat111', 'Rat112'], profile)
+# profile.rewardType = 'Progressive'
+# tagfile = TagFile.TagFile(root, 'Rat111')
+# s = session.batch_get_session_list(root, ['Rat111', 'Rat112'], profile)
+s = session.batch_get_event(root, profile1, profile2)
 # s = tagfile.read_tag_table()
 print(s)
-
