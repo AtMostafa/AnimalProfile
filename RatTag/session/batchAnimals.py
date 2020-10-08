985b21a247828e0e8ce75eed9dcf894531c2f716
mostafa.safaie <mostafa.safaie@gmail.com>
2020-10-08 14:24:07 +0200

    pasted batch_get_event

--- RatTag/session/batchAnimals.py
+++ RatTag/session/batchAnimals.py
@@ -41,4 +41,35 @@ def batch_get_animal_list(root: Root.Root, profile: Profile.Profile = None):
     for session in sessionList:
         animalList.append(session[:len(profile._prefix) + 3])
     animalList = list(set(animalList))
-    return animalList
+    return sorted(animalList)
+
+
+def batch_get_event(root: Root.Root,
+                    profile1: Profile.Profile,
+                    profile2: Profile.Profile,
+                    badAnimals: list = None):
+    """
+    This function finds the animals that match both profile1 and profile2 IN SUCCESSION
+    I.E., when the conditions changed
+    """
+    if badAnimals is None:
+        badAnimals = []
+    animalList1 = batch_get_animal_list(root, profile1)
+    animalList2 = batch_get_animal_list(root, profile2)
+    animalList0 = set(animalList1).intersection(set(animalList2))
+    animalList0 = [animal for animal in animalList0 if animal not in badAnimals]  #remove bad animals from animalList0
+    sessionDic = {key: [[],[]] for key in animalList0}
+    animalList = []
+    for animal in animalList0:
+        sessionListProfile1 = batch_get_session_list(root, animalList=[animal], profile=profile1, until_date='')
+        sessionListProfile2 = batch_get_session_list(root, animalList=[animal], profile=profile2, until_date='')
+        sessionListTotal = batch_get_session_list(root, animalList=[animal], profile={'Type': 'Good'}, until_date='')
+        try:
+            index = sessionListTotal['Sessions'].index(sessionListProfile1['Sessions'][-1])
+            if sessionListProfile2['Sessions'][0] == sessionListTotal['Sessions'][index + 1]:
+                animalList.append(animal)
+                sessionDic[animal][0] = sessionListProfile1['Sessions']
+                sessionDic[animal][1] = sessionListProfile2['Sessions']
+        except Exception as e:
+            logging.warning(repr(e))
+    return animalList, sessionDic
