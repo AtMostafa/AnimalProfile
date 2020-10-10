92ecf19e1335bff5feb75988f450fa72671835e7
mostafa.safaie <mostafa.safaie@gmail.com>
2020-10-10 17:36:47 +0200

    add initialize as _set_up

--- AnimalProfile/Root.py
+++ AnimalProfile/Root.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 import json
+import logging
 from .Profile import Profile
 
 
@@ -33,21 +34,43 @@ class Root:
     def _read_root(self):
         self.settingPath = self.root / self.SETTING_FILE
         if not self.settingPath.is_file():
-            self.initialize()
+            self._set_up()
         with open(self.settingPath, 'r') as f:
             setting = json.load(f)
         self.__dict__.update(setting)
-        self.body.insert(0,'Sessions')
-        self.body.insert(1,'Tag')
+        self.body.insert(0, 'Sessions')
+        self.body.insert(1, 'Tag')
 
-    def initialize(self):
+    def _set_up(self):
         """
         No tag system was found in the 'root' so user is asked
         to provide the necessary information to set everything up
         and write to the 'SETTING_FILE'.
-        This is tun once and at the beginning
+        This is done once and at the beginning
         """
-        raise NotImplementedError('To be added soon')
+        prefix = input("""What is the descriptive prefix of the data structure?\n
+        E.g., Rat in Rat123, Rat124, etc.\n
+        Follow python variable naming rules in all the parameters""")        
+        prefix = prefix.split(maxsplit=2)[0]
+
+        print('Header: parameters defined once for each animal, e.g., name.')
+        Nheader = int(input('\nNumber of header parameters (int):'))
+        header = []
+        for i in range(Nheader):
+            h = input(f'name of header param #{i+1}:')
+            header.append(h.split(maxsplit=2)[0])
+
+        print('Body: parameters defined per session for each animal. "Session":session name and "Tag":experiment tag are added automatically.')
+        Nbody = int(input('\nNumber of body parameters (int):'))
+        body = []
+        for i in range(Nbody):
+            b = input(f'name of body param #{i+1}:')
+            body.append(b.split(maxsplit=2)[0])
+
+        out = {'prefix': prefix, 'header': header, 'body': body}
+        with open(self.settingPath, 'w') as f:
+            json.dump(a, f, indent=4, sort_keys=True)
+        logging.info(f'Written: {self.settingPath}')
 
     def get_profile(self):
         """
