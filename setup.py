115adb73f8afbd2c0b965eac890fa3b9f929d9d9
mostafa.safaie <mostafa.safaie@gmail.com>
2020-10-17 17:43:33 +0200

    add download url

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/atmostafa/AnimalProfile",
+    download_url="https://github.com/AtMostafa/AnimalProfile/archive/0.1.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
